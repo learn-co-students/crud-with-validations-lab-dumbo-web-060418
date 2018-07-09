1247fbbefd6ab4dae7c6dc83b9e416223c2fb519
robertexe <robtabares92@gmail.com>
2018-07-09 07:13:18 -0400

    Done.

---
+++ db/seed.rb
@@ -0,0 +1,3 @@
+Song.create(title: "99", released: 1, release_year: 2000, artist_name:"Jayz", genre: "RAP")
+Song.create(title: "Stronger", released: true, release_year: 2004, artist_name:"Ye", genre: "RAP")
+Song.create(title: "Little Sebastian", released: true, release_year: 2001, artist_name:"Andy Dwyer", genre: "Folk")
